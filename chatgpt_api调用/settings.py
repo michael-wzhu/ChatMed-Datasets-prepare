1177ad06584745a9b15bde2116acacd81ee6c6e5
root <root@instance-1.us-central1-a.c.langaugment.internal>
2023-04-29 03:43:33 +0000

    aaa

--- chatgpt_api调用/settings.py
+++ chatgpt_api调用/settings.py
@@ -16,5 +16,5 @@ api_engine = "gpt-3.5-turbo"
 # api_key = "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"
 # api_key = "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"
 # api_key = "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"
-api_key = "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"
+api_key = "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"
 
