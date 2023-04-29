f31dcf544c6dde7a6c9bfd9d7a3c082dd7e26159
michael <michael_wzhu91@163.com>
2023-04-29 11:39:48 +0800

    0429: prepare self-instruct

--- chatgpt_api调用/39_问诊数据_chatgpt35回答.py
+++ chatgpt_api调用/39_问诊数据_chatgpt35回答.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
 
 
     '''
-    nohup python -u chatgpt_api调用/39_问诊数据_chatgpt35回答.py 91 > 91_log.log &
+    nohup python3 -u chatgpt_api调用/39_问诊数据_chatgpt35回答.py 91 > 91_log.log &
     
     
     
